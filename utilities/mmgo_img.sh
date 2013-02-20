cbd56d49be65bc297624b28396f62e7d88e05c9a
Wee Keat Chin <weekeat@visualdensity.com>
2013-02-20 14:20:09 +1100

    update with thumb

--- utilities/mmgo_img.sh
+++ utilities/mmgo_img.sh
@@ -6,13 +6,14 @@ read pid
 echo "How many images?"
 read number
 
+output_file='/Users/wkeat/Desktop/img_output'
+
 echo "PID: $pid"
 for i in $(seq 1 $number)
 do
-    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_event_hd.jpg"
-    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}.jpg"
-    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}_list.jpg"
-    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}_hd.jpg"
+    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_event_hd.jpg" >> $output_file
+    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}.jpg" >> $output_file
+    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}_list.jpg" >> $output_file
+    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}_hd.jpg" >> $output_file
+    echo "https://www.mumgo.com.au/images/prods/$pid/${pid}_${i}_thumb.jpg" >> $output_file
 done
-
-
